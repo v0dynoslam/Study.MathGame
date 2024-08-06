fa68bdee0237d4958ec1a5bdc06182522764600b
stlam <stlam@DESKTOP-NM7PJFC>
2024-08-05 22:23:18 -0500

    Add project files.

---
+++ MyFirstProgram/Menu.cs
@@ -0,0 +1,64 @@
+﻿namespace MyFirstProgram
+{
+    internal class Menu
+    {
+        GameEngine gameClass = new();
+        internal void ShowMenu(string name, DateTime date)
+        {
+            Console.Clear(); //clear the console everytime the menu runs
+            Console.WriteLine("------------------------");
+            Console.WriteLine($"Hello {name}. It's {date}. This is your math game. It's going to be awesome!");
+            Console.WriteLine("Press any key to show the menu");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear(); //clear the console everytime the menu runs
+                Console.WriteLine($@"
+What type of game would you like to play? Please choose one of the following options  below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit");
+
+                Console.WriteLine("---------------------------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                //Switch statement to select the game operation
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.PrintGames();
+                        break;
+                    case "a":
+                        gameClass.AdditionGame("Addition game");
+                        break;
+                    case "s":
+                        gameClass.SubtractionGame("Subtraction Game");
+                        break;
+                    case "d":
+                        gameClass.DivisionGame("Division Game");
+                        break;
+                    case "m":
+                        gameClass.MultiplicationGame("Multiplication Game");
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye!");
+                        isGameOn = false; //this stops the game
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            } while (isGameOn);
+        }
+
+    }
+}
---
+++ MyFirstProgram/Models/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstProgram.Models;
+
+internal class Game
+{
+    //private int _score; ctrl + k + c to multi comment
+
+    //public int Score
+    //{
+    //    get { return _score; }
+    //    set { _score = value; }
+    //}
+    internal DateTime Date { get; set; }
+    internal int Score { get; set; }
+    internal GameType Type { get; set; }
+}
+
+internal enum GameType
+{
+    Addition,
+    Subtraction,
+    Division,
+    Multiplication
+}
